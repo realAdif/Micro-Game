6ff794f8003b880f2fb59e05607388db30a69c05
Aditya <adityagaikwad693@gmail.com>
2022-10-06 23:04:56 +1100

    Update resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -4,12 +4,37 @@ const {signToken} = require('../utils/auth');
 
 const resolvers ={
     Query:{
-        user: async()=>{
+        users: async()=>{
             return User.find()
         },
         user: async (parent,{username}) =>{
             return User.findOne({username})
         },
-        
+
+    },
+
+    Mutation:{
+        addUser: async(parent, {username, email, password}) => {
+            const user = await User.create({username,email,password});
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, {email,password}) =>{
+            const user = await User.findOne({email}); 
+
+            if(!user){
+                throw new AuthenticationError("No User with this email address");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        }
     }
-}
+};
+
+module.exports = resolvers;
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -19,6 +19,11 @@ const typeDefs = gql`
         user(username: String!): User
     }
 
+    type Mutation{
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
+
 `;
 
 module.exports = typeDefs;
