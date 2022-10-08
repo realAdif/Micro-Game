972dd3637f69ed3870fb33b2fbdc1a8f657c3211
Aditya <adityagaikwad693@gmail.com>
2022-10-08 19:53:20 +1100

    Update client homepage and login

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -6,17 +6,29 @@ import profile from '../styles/asset/ProfilePic.jpg'
 
 function Nav(){
     return(
-        <ul className="nav justify-content-center">
-            <li className="nav-item">
-                <a className="nav-link active" href="#Home">Home</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#MineGame">Mini Game</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/login">Login</a>
-            </li>   
-        </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar10">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navbar-collapse collapse" id="navbar10">
+                    <ul className="navbar-nav nav-fill w-100">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#Home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="MiniGame">Mini Game</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="login">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="signup">signup</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
@@ -61,12 +73,8 @@ export default function Homepage(){
     return(
         <div>
             <img src={banner} alt='bannerImages' id='banner'></img>
-
-            <Nav/>
-        
+            <Nav/>       
             <Profile/>
-
-
         </div>
     )
 }
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -5,8 +5,30 @@ export default function Login(){
 
 
     return(
-        <div>
-            Login
-        </div>
+        <section id="cover" class="min-vh-100">
+            <div id="cover-caption">
+                <div class="container">
+                    <div class="row text-white">
+                        <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
+                            <h1 class="display-4 py-2 text-truncate">login.</h1>
+                            <div class="px-2">
+                                <form action="" class="justify-content-center">
+                                    <div class="form-group">
+                                        <label class="sr-only">username</label>
+                                        <input type="text" class="form-control" placeholder="username"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only">password</label>
+                                        <input type="text" class="form-control"/>
+                                    </div>
+                                    <br/>
+                                    <button type="submit" class="btn btn-primary btn-lg">Launch</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
