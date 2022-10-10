a0c65c479551a894fa5208326434944ca07d99d1
Aditya <adityagaikwad693@gmail.com>
2022-10-10 22:45:08 +1100

    Update client login and app

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,69 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/homePage'
-import Login from './pages/login';
+import Login from './pages/Login';
 import SignUp from './pages/signup';
 import MiniGame from './pages/MiniGames';
 import Hangman from './pages/Hang-Man/Hangman';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-  <Router>
-    <Routes>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
 
-      <Route path='/'
-      element={<Home/>}
-      />
-      <Route path='/minigame'
-      element={<MiniGame/>}
-      />
-      <Route path='/hangman'
-      element={<Hangman/>}
-      />
-      
-      <Route path='/login'
-      element={<Login/>}
-      />
+          <Route path='/'
+          element={<Home/>}
+          />
+          <Route path='/minigame'
+          element={<MiniGame/>}
+          />
+          <Route path='/hangman'
+          element={<Hangman/>}
+          />
+          
+          <Route path='/login'
+          element={<Login/>}
+          />
 
-      <Route path='/signup'
-      element={<SignUp/>}
-      />
-      
-    </Routes>
+          <Route path='/signup'
+          element={<SignUp/>}
+          />
+          
+        </Routes>
 
-  </Router>
+      </Router>
+  </ApolloProvider>
   );
     
 }
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,44 @@
-import React from "react";
-import Navbar from "./Navbar";
-export default function Login(){
+/* eslint-disable no-unused-vars */
+import React,{useState} from "react";
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import {LOGIN_USER} from '../utils/mutations';
+import Auth from '../utils/auth';
 
+import Navbar from "./Navbar";
+
+
+export default function Login(props){
+    const [formState, setFormState] = useState({email: '', password: ''});
+    const [login, {error,data}] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try{
+            const {data} = await login({
+                variables: {...formState},
+            });
+
+            Auth.Login(data.login.token);
+        } catch(e){
+            console.error(e);
+        }
+
+        setFormState({
+            email: '',
+            password: '',
+        });
+    }
 
     return(
         
@@ -13,18 +50,28 @@ export default function Login(){
                         <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
                             <h1 className="display-4 py-2 text-truncate">login</h1>
                             <div className="px-2">
-                                <form action="" className="justify-content-center">
+                                <form action="" className="justify-content-center" onSubmit={handleFormSubmit}>
                                     <div className="form-group">
                                         <label className="sr-only">username</label>
-                                        <input type="text" className="form-control" placeholder="username"/>
+                                        <input type="email" className="form-control" placeholder="username"
+                                        name="email" value={formState.email} onChange={handleChange}
+                                        />
                                     </div>
                                     <div className="form-group">
                                         <label className="sr-only">password</label>
-                                        <input type="text" className="form-control" placeholder="password"/>
+                                        <input type="password" className="form-control" placeholder="password"
+                                        name="password" value={formState.password} onChange={handleChange}
+                                        />
                                     </div>
                                     <br/>
                                     <button type="submit" className="btn btn-primary btn-lg">Launch</button>
                                 </form>
+
+                                {error && (
+                                    <div className="my-3 p-3 bg-danger text-white">
+                                        {error.message}
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
