4b5423c8397222f009919670f978dcc29ed79732
Aditya <adityagaikwad693@gmail.com>
2022-10-24 21:05:19 +1100

    deploying

--- client/src/pages/MiniGames.js
+++ client/src/pages/MiniGames.js
@@ -11,39 +11,6 @@ import mathQuiz from '../styles/asset/MathQuiz.png'
 export default function MiniGame(){
 
     return(
-        // <section>
-        //     <Navbar/>
-        //     <br/>
-        //     <Container fluid id="container" >
-        //         <Row justify="between" debug>
-        //             <Col >
-        //             <div className="card">
-        //             <img src={hangman} className="card-img-top" alt='bannerImages' id='banner'></img>
-        //                 <div className="card-body">
-        //                     <h5 className="card-title">Hangman</h5>
-        //                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //                     <a href="/hangman" className="btn btn-primary">Lets play</a>
-        //                 </div>
-        //             </div>
-        //             </Col>
-        //             <Col >
-        //             <div className="card">
-        //             <img src={mathQuiz} className="card-img-top" alt='bannerImages' id='banner'></img>
-        //                 <div className="card-body">
-        //                     <h5 className="card-title">Math Quiz</h5>
-        //                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //                     <a href="/mathquiz" className="btn btn-primary">Go somewhere</a>
-        //                 </div>
-        //             </div>  
-        //             </Col>
-        //         </Row>
-        //         <br/>
-        //     </Container>
-        //     <p id="minigameText"> More Micro-Games are on the way...</p>
-
-
-        //     <Footer/>
-        // </section>
         <>
         <Navbar/>
         <Container>
@@ -73,8 +40,8 @@ export default function MiniGame(){
                             Math Quiz
                         </Item.Description>
                         <Item.Extra>
-                        <Button floated='right' href='/mathquiz' primary>
-                        Lets Play
+                        <Button floated='right' href='/' disabled>
+                        Coming Soon!!
                             <Icon name='chevron right' />
                         </Button>
                         </Item.Extra>
