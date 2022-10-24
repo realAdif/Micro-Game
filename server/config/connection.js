505dec95775a6330d91d81337fe7dde65f13d92b
Aditya <adityagaikwad693@gmail.com>
2022-10-24 21:58:25 +1100

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/microgamedb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
